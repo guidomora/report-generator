ff9a6b81e4e6b7443d77e08c1e755c4b78461900
guido morabito <guidomorabito161@hotmail.com>
2024-10-21 22:50:54 -0300

    added

--- src/app/todoModa/page.tsx
+++ src/app/todoModa/page.tsx
@@ -2,81 +2,69 @@ import Link from "next/link"
 
 // TODO: crear un array con las opciones para mapear y crear los inputs
 
+const formFields = [
+    { label: "Título de la sección", type: "text", placeholder: "titulo", multiple: false, isFile: false },
+    { label: "Salon", type: "file", placeholder: "imágenes", multiple: true, isFile: true },
+    { label: "Trastienda / baño", type: "file", placeholder: "imágenes", multiple: true, isFile: true },
+    { label: "Luminarias", type: "file", placeholder: "imágenes", multiple: true, isFile: true },
+    { label: "Aire acondicionado", type: "file", placeholder: "imágenes", multiple: true, isFile: true },
+    { label: "Filtros Aire acondicionado", type: "file", placeholder: "imágenes", multiple: true, isFile: true },
+    { label: "Matafuegos", type: "file", placeholder: "imágenes", multiple: true, isFile: true },
+    { label: "Tablero Eléctrico", type: "file", placeholder: "imágenes", multiple: true, isFile: true },
+    { label: "Otras tareas", type: "file", placeholder: "imágenes", multiple: true, isFile: true },
+    {
+        label: "Materiales Utilizados",
+        type: "text",
+        multipleInputs: [
+            { placeholder: "detalle", type: "text" },
+            { placeholder: "cantidad", type: "text" },
+            { placeholder: "imagenes", type: "file" },
+        ],
+        multiple: false,
+        isFile: true,
+    },
+    { label: "Sugerencias de mejoras y tareas pedientes", type: "file", placeholder: "imágenes", multiple: true, isFile: true },
+    { label: "Remito firmado", type: "file", placeholder: "imágenes", multiple: true, isFile: true },
+];
+
 
 const TodoModa = () => {
     return (
-        <div className='h-screen w-screen flex flex-col justify-center items-center'>
-            <div className="mb-4 flex justify-center items-baseline">
+        <div className='flex flex-col justify-center items-center'>
+            <div className="my-12 flex justify-center items-baseline">
                 <h1 className="mr-3 font-bold">Generar informe de Todo Moda / Isadora</h1>
                 <Link href={"/"}>
                     <button className="px-4 p h-8 bg-blue-600 text-white rounded-xl mt-6">Pagina principal</button>
                 </Link>
             </div>
+            <div className="flex flex-col justify-around items-center h-full w-[90%]   gap-5">
+                {formFields.map((field, index) => (
+                    <div key={index} className="flex justify-between items-center p-6 w-[30%] border border-slate-400 rounded-xl">
+                        <span className="underline text-center m-2 w-[40%]">{field.label}</span>
 
-            <div className="p-6 flex justify-center  items-center flex-wrap flex-row h-[90%] w-[90%] border border-black rounded-xl">
-
-                <div className='flex flex-col w-full justify-center items-center'>
-                    <span className="underline mt-2">Título de la sección</span>
-                    <input className='mt-2 border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md' type="text" placeholder='titulo' />
-                </div>
-                <hr className='mt-4' />
-                <h2>Secciones:</h2>
-                <div className='flex flex-col'>
-                    <span className="underline mt-2">Salon</span>
-                    <input type='file' multiple placeholder='imágenes' className='mt-2' />
-                </div>
-                <div className='flex flex-col'>
-                    <span className="underline mt-2">Trastienda / baño </span>
-                    <input type='file' multiple placeholder='imágenes' className='mt-2' />
-                </div>
-                <div className='flex flex-col'>
-                    <span className="underline mt-2">Luminarias </span>
-                    <input type='file' multiple placeholder='imágenes' className='mt-2' />
-                </div>
-                <div className='flex flex-col'>
-                    <span className="underline mt-2">Luminarias </span>
-                    <input type='file' multiple placeholder='imágenes' className='mt-2' />
-                </div>
-                <div className='flex flex-col'>
-                    <span className="underline mt-2">Aire acondicionado </span>
-                    <input type='file' multiple placeholder='imágenes' className='mt-2' />
-                </div>
-                <div className='flex flex-col'>
-                    <span className="underline mt-2">Filtros Aire acondicionado </span>
-                    <input type='file' multiple placeholder='imágenes' className='mt-2' />
-                </div>
-                <div className='flex flex-col'>
-                    <span className="underline mt-2">Matafuegos </span>
-                    <input type='file' multiple placeholder='imágenes' className='mt-2' />
-                </div>
-                <div className='flex flex-col'>
-                    <span className="underline mt-2">Tablero Eléctrico </span>
-                    <input type='file' multiple placeholder='imágenes' className='mt-2' />
-                </div>
-                <div className='flex flex-col'>
-                    <span className="underline mt-2">Aire acondicionado </span>
-                    <input type='file' multiple placeholder='imágenes' className='mt-2' />
-                </div>
-                <div className='flex flex-col'>
-                    <span className="underline mt-2">Otras tareas </span>
-                    <input type='file' multiple placeholder='imágenes' className='mt-2' />
-                </div>
-                <div className='flex flex-col'>
-                    <span className="underline mt-2">Materiales Utilizados </span>
-                    <div className='flex flex-row'>
-                        <input className='mt-2 border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md' type="text" placeholder='detale' />
-                        <input className='mt-2 border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md' type="text" placeholder='cantidad' />
+                        {field.multipleInputs ? (
+                            <div className="flex justify-between">
+                                <div className="flex flex-col">
+                                    {field.multipleInputs.map((input, i) => (
+                                        <input
+                                            key={i}
+                                            type={input.type}
+                                            placeholder={input.placeholder}
+                                            className="w-full mt-2 border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md"
+                                        />
+                                    ))}
+                                </div>
+                            </div>
+                        ) : (
+                            <input
+                                className="w-[60%] mt-2 border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md"
+                                type={field.type}
+                                placeholder={field.placeholder}
+                                multiple={field.multiple}
+                            />
+                        )}
                     </div>
-                    <input type='file' multiple placeholder='imágenes' className='mt-2' />
-                </div>
-                <div className='flex flex-col'>
-                    <span className="underline mt-2">Sugerencias de mejoras y tareas pedientes </span>
-                    <input type='file' multiple placeholder='imágenes' className='mt-2' />
-                </div>
-                <div className='flex flex-col'>
-                    <span className="underline mt-2">Remito firmado </span>
-                    <input type='file' multiple placeholder='imágenes' className='mt-2' />
-                </div>
+                ))}
             </div>
             <button className="px-4 p h-8 bg-blue-600 text-white rounded-xl my-6">Generar Informe</button>
         </div>
